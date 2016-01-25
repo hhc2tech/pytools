f16b11780b873d663aaa77d3def1a14b3cf45e9c
Neal Gordon <nealagordon@gmail.com>
2016-01-24 20:49:53 -0800

    add flags to pyver

--- pyver/README.md
+++ pyver/README.md
@@ -1,18 +1,43 @@
+# pyver 
 Version control so simple your mom can do it.
 
 The movtiation behind this attempt at creating a version control method is to create a very, very simple way to track the history of files. The work-flow of pyver is something I do anyway when working on something simple that uses binary files, especially ones that are linked through the software that is required to read them (eg CAD designs). I will create multiple directories, eg v1, v2 and copy the entire contents of my in-progress files in it that I want to preserve the history.
 
-The basic operation of this program goes like this
+## The basic operation of this program goes like this
 1) create some files
 2) run pyver and make a revision
 3) make as many revisions as you want
-4) if you want to revert back to an older revision, checkout an older one or copy the files from ./.pyver/vx and put them in your current diretory
+4) if you want to revert back to an older revision, checkout an older one or copy the files from .pyver/folderversion and put them in your current diretory
 
-Notes
-* all the files in the directory are stored when a revision is made. Although this is inefficient for large files, it keeps it dead simple and users can always just copy and paste their revision somewhere else. There is no learning curve with syntax.
-
-Criteria for design
-* cannot rename files to track versions. This accomplishes this by putting the files in different directories
+### Criteria for design
+* It is unacceptable to rename files to track versions. This accomplishes this by putting the files in different directories and keep your current file with the desired, persistent filename
 * Can be used over a network drive. Does not require a central server. 
 * Be OS agnostic (written in Python)
 * know when the changes were made, with a comment and who did it
+
+# Tutorial
+To get started, lets do a quick demo in windows. In your current directory, open a command line with ```shift+right_click``` and select ```Open command window here```
+
+Now type 
+```
+pv
+```
+
+Done. We have created a pyver repository and made a backup of all the files in our current directory. Anytime we need to make another backup, just repeat the last step.
+
+There are a few built in commands to interrogate the repository. The first is log, which shows the contents of a text file that keeps track of the commits that are made
+```
+pv log
+```
+
+The next is tree, which shows all the files in the repo.
+```
+pv tree
+```
+
+
+We also have some flags we can use to customize our commits. This commit adds only the files file1.txt and file2.docx and adds a comment. Note-none of the flag inputs can have spaces, so files are seperated by ```|``` and comments are written without spaces using ```-```
+
+```
+pv -f file1.txt|file2.docx -c added-figures-to-docx-and-updated-text-with-numbers
+```
--- pyver/pyver.py
+++ pyver/pyver.py
@@ -7,63 +7,121 @@ add files to the folder, save a revision, or checkout a revision
 archive naming year-month-day-hour-minute-second
 
 http://pythoncentral.io/how-to-recursively-copy-a-directory-folder-in-python/
-
 https://docs.python.org/3/library/shutil.html#shutil.ignore_patterns
+
 
 """
 
-import os, shutil, datetime, sys
+import os, shutil, datetime, sys, argparse
 
 
-def pyver(user, comment):
+def pyver(user, archivefiles, comment):
     '''
-    user = 'ngordon'
-    comment = 'added some more files'
+	main function of pyver. copys files from the current directory and creates an archive.
+	the tab-delimmeted log file pyver.csv keeps a record of the file changes
+    '''
     
-    '''
+    if not os.path.isdir('.pyver'):
+        os.mkdir('.pyver')
+        make_hidden('.pyver')
+        
     n = datetime.datetime.now()
     timestamp = datetime.datetime.strftime(n, '%Y%m%d%H%M%S')  
-    archivepath = '.pyver\\'+timestamp   
-    shutil.copytree('.', archivepath, ignore=shutil.ignore_patterns('.pyver'))
-    archivefiles = os.listdir(archivepath)
+    archivepath = '.pyver\\'+timestamp  
+    os.mkdir(archivepath)
+
+	### was used to copy an entire directory but was wasteful
+    #archivefiles = os.listdir(archivepath)
+    #shutil.copytree('.', archivepath, ignore=shutil.ignore_patterns('.pyver'))
+
+    for f in archivefiles:
+        print(f)
+        shutil.copy(f , archivepath+'\\'+f)
+    
     archivesize = sum([os.path.getsize(s) for s in os.listdir(archivepath)])/1e3 # kb
     
-    #makes a zip file instead
-    shutil.make_archive(archivepath,'zip', archivepath)
-    shutil.rmtree(archivepath)
+    ### makes a zip file instead
+    #shutil.make_archive(archivepath,'zip', archivepath)
+    #shutil.rmtree(archivepath)
 
     archivefilesstr = ' '.join(archivefiles)
     ''' writes what files were archived'''
     pyverlog = open('.pyver\\pyver.log','a')
-    pyverlog.write('%s-%s-%s-%ikb-%s\n' % (user,timestamp,comment,archivesize,archivefilesstr))
+    pyverlog.write('%s , %s , %ikb , %s , %s\n' % (user,
+                                           timestamp,
+                                           archivesize,
+                                           comment,
+                                           archivefilesstr))
     pyverlog.close()
-
-def make_hidden():
+	
+def make_hidden(hidedir):
     '''creates windows hidden folder
     can also use the windows command 
     os.system("attrib +s +h "+.pyver)
     '''
     import ctypes
     FILE_ATTRIBUTE_HIDDEN = 0x02
-    ret = ctypes.windll.kernel32.SetFileAttributesW("C:\\Users\\Neal\\Desktop\\pyver\\.pyver", FILE_ATTRIBUTE_HIDDEN)
+    ret = ctypes.windll.kernel32.SetFileAttributesW(hidedir, FILE_ATTRIBUTE_HIDDEN)
     if ret:
-        print('attribute set to Hidden')
+        print('.pyver set to Hidden')
     else:  # return code of zero indicates failure, raise Windows error
         raise ctypes.WinError()
 
-def show_log():
-    pyverlog = open('pyver\\pyver.log','r')
-    for k in pyverlog.readlines():
-        print(k)
-    pyverlog.close()
+def log():
+	'''shows the contents of the pyver log file'''
+    if os.path.exists('.pyver\\pyver.log'):
+        pyverlog = open('.pyver\\pyver.log','r')
+        for k in pyverlog.readlines():
+            print(k)
+        pyverlog.close()
+    else:
+        print('not a pver repository')
 
+def tree(path,indent=' '):
+    '''recursiveley prints the contents of a directory'''
+    for file in os.listdir(path):
+        fullpath = path + '/' + file              
+        if os.path.isfile(fullpath):
+            print('%s     %s  ' % (indent,file ) )
+        else:   # os.path.isdir
+            print('%s  ./%s ' % (indent,file ) )
+            tree(fullpath,indent + '   |')
 
 if __name__=='__main__':
     '''
-    pass arguments to indicate user and comment
-    pyver neal testing 
+	executed if module is not imported
     '''
-    user = sys.argv[1]
-    comment = sys.argv[2]    
-    pyver(user, comment)
+    
 
+    if len(sys.argv) == 2 and sys.argv[1] == 'log':
+        log()
+    elif len(sys.argv) == 2 and sys.argv[1] == 'tree':
+        tree('.')
+    else:    
+             
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-f', '--my-foo', default='foobar')
+        parser.add_argument('-b', '--bar-value', default=3.14)
+       
+        p = argparse.ArgumentParser()
+        p.add_argument('-u', '--user', default = os.path.split(os.path.expanduser('~'))[-1], 
+                       help='user that made the pyver entry')
+        p.add_argument('-f', '--files',
+                       help='add files separated by vertical line, example, file1.txt|file2.txt')
+        p.add_argument('-c', '--comment', default = '', 
+                       help='add a comment as to what changed')
+        args = p.parse_args()
+        
+        if args.files:
+            tempfiles = args.files.split('|')
+            args.files = []
+            for t in tempfiles:
+                if t not in os.listdir(os.getcwd()):
+                    print('%s not found, skipping' % t)
+                else:
+                    args.files.append(t)
+        else:
+            args.files = os.listdir(os.getcwd())
+            if '.pyver' in args.files:
+                args.files.remove('.pyver')
+        pyver(args.user, args.files, args.comment)      
