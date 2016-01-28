852f6084ccc83cf1186b9d40cc95f3e3a34c48bf
Neal Gordon <ngordon@engineering.scaled.com>
2016-01-27 21:26:55 -0800

    found an error in pyver that is does not copy files in subdirectories so I reverted to copyall and will figure it out later

--- dirtree/dirtree_simple.py
+++ dirtree/dirtree_simple.py
@@ -16,3 +16,17 @@ def print_tree(path,indent=' '):
         else:   # os.path.isdir
             print('%s  ./%s ' % (indent,file ) )
             print_tree(fullpath,indent + '   |')
+            
+def all_files(path,indent=' '):
+    # when dirsize=True, the directory size is calculated, but can take a long time!
+    # recursiveley prints the contents of a directory
+    files = []
+    for file in os.listdir(path):
+        fullpath = os.path.join(path,file)
+        if os.path.isfile(fullpath):
+            print('%s     %s  ' % (indent,file ) )
+            files.append(file)
+        else:   # os.path.isdir
+            print('%s  ./%s ' % (indent,file ) )
+            print_tree(fullpath,indent + '   |')            
+
--- pyver/pyver.py
+++ pyver/pyver.py
@@ -8,12 +8,12 @@ __version__ = '0.1'
 
 import os, shutil, datetime, sys, argparse
 
-def pyver(user, archivefiles, comment):
+def pyver(user, comment):# archivefiles, ):
     '''
 	main function of pyver. copys files from the current directory and creates an archive.
 	the tab-delimmeted log file pyver.csv keeps a record of the file changes
     '''
-    
+    archivefiles = []
     if not os.path.isdir('.pyver'):
         os.mkdir('.pyver')
         try:
@@ -24,15 +24,19 @@ def pyver(user, archivefiles, comment):
     n = datetime.datetime.now()
     timestamp = datetime.datetime.strftime(n, '%Y%m%d%H%M%S')  
     archivepath = os.path.join('.pyver',timestamp)
-    os.mkdir(archivepath)
-
+    
+   
 	### was used to copy an entire directory but was wasteful
-    #archivefiles = os.listdir(archivepath)
-    #shutil.copytree('.', archivepath, ignore=shutil.ignore_patterns('.pyver'))
 
-    for f in archivefiles:
-        print(f)
-        shutil.copy(f , os.path.join(archivepath, f))
+    shutil.copytree('.', archivepath, ignore=shutil.ignore_patterns('.pyver'))
+
+    # does not copy sub-directories
+#    os.mkdir(archivepath)
+#    for f in archivefiles:
+#        print(f)
+#        print(os.path.join(archivepath, f))
+#        shutil.copy(f , os.path.join(archivepath, f))
+    
     
     archivesize = sum([os.path.getsize(s) for s in os.listdir(archivepath)])/1e3 # kb
     
@@ -90,46 +94,57 @@ def make_win_exe():
     '''
     os.system('pip install pyinstaller')
     os.system('pyinstaller pyver.py --onefile')    
-    
-        
+
+def all_files(rootDir = '.'):
+    #rootDir = '.'
+    files = []
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        print(dirName)
+        print(subdirList)
+        print(fileList)
+        for fname in fileList:
+            fullpath = os.path.join(dirName, fname)
+            if fullpath.split(os.sep)[1] != '.pyver':
+                print(fullpath)    
+                files.append(fullpath[2:])
+    return files
+
 if __name__=='__main__':
     '''
 	executed if module is not imported
     '''
     
-
     if len(sys.argv) == 2 and sys.argv[1] == 'log':
         log()
     elif len(sys.argv) == 2 and sys.argv[1] == 'tree':
         tree('.')
     else:    
-             
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-f', '--my-foo', default='foobar')
-        parser.add_argument('-b', '--bar-value', default=3.14)
        
         p = argparse.ArgumentParser()
         p.add_argument('-u', '--user', 
                        default = os.path.split(os.path.expanduser('~'))[-1], 
                        help='user that made the pyver entry')
-        p.add_argument('-f', '--files',
-                       help='''add files separated by vertical line with no  
-                               spaces, example, file1.txt|file2.txt''')
+#        p.add_argument('-f', '--files',
+#                       help='''add files separated by vertical line with no  
+#                               spaces, example, file1.txt|file2.txt''')
         p.add_argument('-c', '--comment', default = '', 
                        help='''add a comment enclosed in double quotes 
                                as to what changed.''')
         args = p.parse_args()
         
-        if args.files:
-            tempfiles = args.files.split('|')
-            args.files = []
-            for t in tempfiles:
-                if t not in os.listdir(os.getcwd()):
-                    print('%s not found, skipping' % t)
-                else:
-                    args.files.append(t)
-        else:
-            args.files = os.listdir(os.getcwd())
-            if '.pyver' in args.files:
-                args.files.remove('.pyver')
-        pyver(args.user, args.files, args.comment)      
+#        if args.files:
+#            tempfiles = args.files.split('|')
+#            args.files = []
+#            for t in tempfiles:
+#                if t not in os.listdir(os.getcwd()):
+#                    print('%s not found, skipping' % t)
+#                else:
+#                    args.files.append(t)
+#        else:
+#            #files = os.listdir(os.getcwd())  ## does not capture subdirectories
+#            args.files = all_files('.')
+#            if '.pyver' in args.files:
+#                args.files.remove('.pyver')
+        #user, archivefiles, comment = args.user, args.files, args.comment        
+        pyver(args.user, args.comment) #args.files, )      
+        
