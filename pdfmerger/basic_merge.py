836075eafd54cf455f7efe6dfeac8dfa608bcfb3
Neal Gordon <nealagordon@gmail.com>
2016-06-18 17:19:33 -0700

    rearraged pypdfmerger

--- pdfmerger/basic_merge.py
+++ pdfmerger/basic_merge.py
@@ -2,25 +2,46 @@
 from PyPDF2 import PdfFileMerger
 merger = PdfFileMerger(strict = False)
 
-dir1 = 'C:/Users/ngordon/Desktop/'
+dir1 = 'C:/Users/Neal/Desktop/US Marine Corps Private Pilot Ground School/'
 
-file1 = '1.pdf'
-file2 = '2.pdf'
+file1 = 'USMCI_Airplane Instruments and Accessories_Part 1.pdf'
+file2 = 'USMCI_Airplane Instruments and Accessories_Part 2.pdf'
 
 input1 = open(dir1+file1, "rb")
 input2 = open(dir1+file2, "rb")
 
 # add the first 3 pages of input1 document to output
 #merger.append(fileobj = input1, pages = (0,3))
-merger.append(fileobj = input1)
-merger.append(fileobj = input2)
+merger.append(fileobj=input1)
+merger.append(fileobj=input2)
 
 # Write to an output PDF document
-output = open("document-output.pdf", "wb")
+output = open("C:/Users/Neal/Desktop/output3.pdf", "wb")
 
 merger.write(output)
 
 
+
+"""
+PDF merger. Loops throughs all the files in files_dir and merges any pdfs
+https://pythonhosted.org/PyPDF2/PdfFileMerger.html
+
+pip install PyPDF2
+
+"""
+
+import os
+from PyPDF2 import PdfFileReader, PdfFileMerger
+
+files_dir = "D:/Astronautics Structures Manual"
+pdf_files = [f for f in os.listdir(files_dir) if f.endswith("pdf")] # glob.glob('*.pdf')
+
+merger = PdfFileMerger(strict = False)
+
+for filename in pdf_files:
+    merger.append(PdfFileReader(os.path.join(files_dir, filename), "rb"), bookmark=filename)
+
+merger.write(os.path.join(files_dir, "Astronautics Structures Manual.pdf"))
 
 
 
@@ -58,22 +79,5 @@ pdfOutput.close()
 
 
 
-"""
-Created on Sun Mar 27 20:56:51 2016
 
-@author: Neal
-https://pythonhosted.org/PyPDF2/PdfFileMerger.html
-"""
 
-import os
-from PyPDF2 import PdfFileReader, PdfFileMerger
-
-files_dir = "C:/Users/Neal/Desktop/US Marine Corps Private Pilot Ground School"
-pdf_files = [f for f in os.listdir(files_dir) if f.endswith("pdf")] # glob.glob('*.pdf')
-
-merger = PdfFileMerger(strict = False)
-
-for filename in pdf_files:
-    merger.append(PdfFileReader(os.path.join(files_dir, filename), "rb"), bookmark=filename)
-
-merger.write(os.path.join(files_dir, "US Marine Corps Private Pilot Ground School.pdf"))
